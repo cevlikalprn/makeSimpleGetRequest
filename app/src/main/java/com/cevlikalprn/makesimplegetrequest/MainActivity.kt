040037fa4fb7901c3378a58539f08d807755aa97
cevlikalprn <devcalprn@gmail.com>
2022-12-08 22:01:50 +0300

    add new line

--- app/src/main/java/com/cevlikalprn/makesimplegetrequest/MainActivity.kt
+++ app/src/main/java/com/cevlikalprn/makesimplegetrequest/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-    //test
+    //test2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
